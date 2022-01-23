096467e892028b2f2cfae425f61bce4b69139669
11x1 <89593923+11x1@users.noreply.github.com>
2022-01-23 12:51:57 -0800

    Update main.js

--- shibe.host custom theme/main.js
+++ shibe.host custom theme/main.js
@@ -34,25 +34,86 @@ theme_dropdown.addEventListener('change', function() {
     }
 })
 
+// setup end
+
+
+// Dropdown options
+// dropdown menu for pages
 let newdropdown = document.createElement('select')
 newdropdown.className = 'w-full select select-bordered'
 custom_theme_div.append(newdropdown)
 
-let header_option = document.createElement('option')
-header_option.value = 'Header'
-header_option.innerText = 'Header'
-newdropdown.append(header_option)
+// Home page option
+let home_option = document.createElement('option')
+home_option.value = 'home_option'
+home_option.innerText = 'Home'
+newdropdown.append(home_option)
 
-let body_option = document.createElement('option')
-body_option.value = 'Body'
-body_option.innerText = 'Body'
-newdropdown.append(body_option)
+// Uploads page option
+let uploads_option = document.createElement('option')
+uploads_option.value = 'uploads_option'
+uploads_option.innerText = 'Uploads'
+newdropdown.append(uploads_option)
 
-let header_options_div = document.createElement('div')
-header_options_div.id = 'header_options'
-header_options_div.style.display = 'block'
-custom_theme_div.append(header_options_div)
+// About page option
+let about_option = document.createElement('option')
+about_option.value = 'about_option'
+about_option.innerText = 'About'
+newdropdown.append(about_option)
 
+// About page option
+let settings_option = document.createElement('option')
+settings_option.value = 'settings_option'
+settings_option.innerText = 'Settings'
+newdropdown.append(settings_option)
+
+// About page option
+let upload_option = document.createElement('option')
+upload_option.value = 'upload_option'
+upload_option.innerText = 'Upload'
+newdropdown.append(upload_option)
+
+// About page option
+let general_option = document.createElement('option')
+general_option.value = 'general_option'
+general_option.innerText = 'General'
+newdropdown.append(general_option)
+// dropdown options end
+
+
+// Div for each option
+let home_option_div = document.createElement('div')
+home_option_div.id = 'home_option_div'
+home_option_div.style.display = 'block'
+custom_theme_div.append(home_option_div)
+
+let uploads_option_div = document.createElement('div')
+uploads_option_div.id = 'uploads_option_div'
+uploads_option_div.style.display = 'block'
+custom_theme_div.append(uploads_option_div)
+
+let about_option_div = document.createElement('div')
+about_option_div.id = 'about_option_div'
+about_option_div.style.display = 'block'
+custom_theme_div.append(about_option_div)
+
+let settings_option_div = document.createElement('div')
+settings_option_div.id = 'settings_option_div'
+settings_option_div.style.display = 'block'
+custom_theme_div.append(settings_option_div)
+
+let upload_option_div = document.createElement('div')
+upload_option_div.id = 'upload_option_div'
+upload_option_div.style.display = 'block'
+custom_theme_div.append(upload_option_div)
+
+let general_option_div = document.createElement('div')
+about_option_div.id = 'general_option_div'
+general_option_div.style.display = 'block'
+custom_theme_div.append(general_option_div)
+// Divs for each option end
+
+// Help text
 let small_help_text = document.createElement('p')
 small_help_text.className = 'text-xs mt-2 text-center'
 small_help_text.innerText = 'Change your custom theme colors. Use the dropdown to select a specific tab.'
@@ -82,13 +143,32 @@ function a(parent, innerHTML, target_elem_color, target_elem_bg) {
     parent.append(custom_elements[innerHTML]['color'])
 }
 
-// create a header bg changer element
+// header bg
 a(header_options_div, 'Header background ', undefined, document.getElementsByClassName('navbar mb-2 flex justify-between shadow-lg bg-neutral text-neutral-content')[0])
+// header text
+a(header_options_div, ' Header text ', document.getElementsByClassName('navbar mb-2 flex justify-between shadow-lg bg-neutral text-neutral-content')[0], undefined)
 
 // handle visibility of shit
 newdropdown.addEventListener('change', function() {
-    header_options_div.style.display = 'none'
-    if (newdropdown.value == 'Header') {
-        header_options_div.style.display = 'block'
+    home_option_div.style.display = 'none'
+    uploads_option_div.style.display = 'none'
+    about_option_div.style.display = 'none'
+    settings_option_div.style.display = 'none'
+    upload_option_div.style.display = 'none'
+    general_option_div.style.display = 'none'
+    
+    
+    if (newdropdown.value == 'home_option') {
+        home_option_div.style.display = 'block'
+    } else if (newdropdown.value == 'uploads_option') {
+        uploads_option_div.style.display = 'block'
+    } else if (newdropdown.value == 'about_option') {
+        about_option_div.style.display = 'block'
+    } else if (newdropdown.value == 'settings_option') {
+        settings_option_div.style.display = 'block'
+    } else if (newdropdown.value == 'upload_option') {
+        upload_option_div.style.display = 'block'
+    } else if (newdropdown.value == 'general_option') {
+        general_option_div.style.display = 'block'
     }
 })
